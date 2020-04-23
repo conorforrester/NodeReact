22f553c0e46c9793ddf90f04255e6e9c09e851d5
Conor Forrester <conor.forrester214@gmail.com>
2020-04-23 07:34:25 -0400

    Create subdoc collection in surveyRoutes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,10 +1,23 @@
+const mongoose = require('mongoose');
 //use requireLogin logic to check if user is first logged in
 const requireLogin = require('../middlewares/requireLogin');
 //use requireCredits logic to check if user has enough credits
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-        
+        const { title, subject, body, recipients } = req.body;
+
+        const survey = new Survey({
+            title,  //ES6 syntax to condense "title: title" to "title"
+            subject,
+            body,
+            //split email addresses into array of strings and map into array of objects
+            recipients: recipients.split(',').map(email => { return { email: email }}),
+            _user: req.user.id,
+            dateSent: Date.now()
+        });
     });
 }
