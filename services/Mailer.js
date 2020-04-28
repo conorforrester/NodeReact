1136717870f86a4d1002deb791a9eea246b2f923
Conor Forrester <conor.forrester214@gmail.com>
2020-04-28 08:37:49 -0400

    Correction to from_email in Mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -8,7 +8,7 @@ class Mailer extends helper.Mail {
         super();
 
         this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.from_email = new helper.Email('conor.forrester214@gmail.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
