d4fb3ca35b78753f4962f352089be9dde367d3d4
Conor Forrester <conor.forrester214@gmail.com>
2020-04-28 07:55:31 -0400

    Mailer and sendgrid code

--- middlewares/requireCredits.js
+++ middlewares/requireCredits.js
@@ -1,7 +1,7 @@
 //central logic for checking if we have enough credits to send a survey
 //stop the middleware process and do NOT proceed to the route handler
 module.exports = (req, res, next) => {
-    if (!req.user.credits < 1) {
+    if (req.user.credits < 1) {
         return res.status(403).send({ error: 'You do not have enough credits' });
     }
 
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 //use requireCredits logic to check if user has enough credits
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -15,9 +17,12 @@ module.exports = app => {
             subject,
             body,
             //split email addresses into array of strings and map into array of objects
-            recipients: recipients.split(',').map(email => { return { email: email }}),
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
             _user: req.user.id,
             dateSent: Date.now()
         });
+
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        mailer.send();
     });
 }
