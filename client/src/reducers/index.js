cdda3d99103c8cf20493bc3c941bff57a790e99a
Conor Forrester <conor.forrester214@gmail.com>
2020-05-01 07:46:03 -0400

    Redux Form setup, Dashboard and SurveyNew components setup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
     //whatever keys we provide, represent the keys that exist inside of state object 
-    auth: authReducer   //auth piece of state is being created by authReducer
+    auth: authReducer,   //auth piece of state is being created by authReducer
+    form: reduxForm
 });
