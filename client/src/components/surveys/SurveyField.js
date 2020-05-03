4c613d093da4e16d54a9b6000344eeb1aa4130ea
Conor Forrester <conor.forrester214@gmail.com>
2020-05-03 12:55:54 -0400

    Create SurveyField component and wire up to SurveyForm

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,12 @@
+// SurveyField contains logic to render a single lable and text input
+
+import React from 'react';
+
+export default ({ input }) => {
+
+    return (
+        <div>
+            <input {...input} />
+        </div>
+    );
+};
