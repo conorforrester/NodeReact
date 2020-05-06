c76f86b3639c71190e61a8ed4d8b7c51219c09e4
Conor Forrester <conor.forrester214@gmail.com>
2020-05-06 07:30:46 -0400

    Set up the SurveyFormReview component

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const SurveyReview = () => {
+    return (
+        <div>
+            <h5>Please Review Your Entries</h5>
+        </div>
+    )
+}
+
+export default SurveyReview;
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,33 @@
-//SurveyNew shows SurveyForm and SurveyFormNew
+//SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = { new: true };
+    // }
+
+    state = { showFormReview: false };  //identical to above constructor
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview />;
+        }
+
+        return (
+            <SurveyForm 
+                onSurveySubmit = {() => this.setState({ showFormReview: true })} 
+            />
+        );
+    }
+
     render () {
         return (
             <div>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         );
     }
