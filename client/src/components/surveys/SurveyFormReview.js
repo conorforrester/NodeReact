74af909f9e16fa425a825555c4cac5301eb99ab0
Conor Forrester <conor.forrester214@gmail.com>
2020-05-06 08:35:35 -0400

    Dumping form values in SurveyNew

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,17 +1,41 @@
 //SurveyFormReview shows users their form inputs for review
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+
+    const reviewFields = _.map(formFields, ({ name, label }) => {
+        return (
+            <div key={name}>
+                <label>
+                    {label}
+                </label>
+                <div>
+                    {formValues[name]}
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <h5>Please Review Your Entries</h5>
+            {reviewFields}
             <button
                 className = "yellow darken-3 btn-flat"
                 onClick={onCancel}
             >
             Back
+            </button>
+            <button
+                onClick={() => submitSurvey(formValues)} 
+                className="green btn-flat right white-text"
+            >
+            Send Survey
+            <i className="material-icons right">email</i>
             </button>
         </div>
     )
@@ -23,4 +47,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
