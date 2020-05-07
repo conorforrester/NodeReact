b3e8c3bb75a9b546ce8e37cbb0e1ffcdadd36424
Conor Forrester <conor.forrester214@gmail.com>
2020-05-07 08:00:26 -0400

    Set up Webhooks, route parsing, take care of duplicate events

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 //use requireLogin logic to check if user is first logged in
 const requireLogin = require('../middlewares/requireLogin');
@@ -12,6 +15,25 @@ module.exports = app => {
 
     app.get('/api/surveys/thanks', (req, res) => {
         res.send('Thanks for voting!');
+    });
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const events = _.map(req.body, ({ email, url }) => {
+            const pathname = new URL(url).pathname; //extract pathname from route
+            const p = new Path('/api/surveys/:surveyId/:choice');   //extract survey ID and choice from pathname
+            const match = p.test(pathname); //will either return an object, or null
+            if (match) {
+                return { email, surveyId: match.surveyId, choice: match.choice};
+            }
+        });
+        //remove undefined elements, return only event objects
+        const compactEvents = _.compact(events);
+        //go through compactEvents, look at email and surveyId property, remove duplicates
+        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+        console.log(uniqueEvents);
+
+        res.send({});
     });
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
